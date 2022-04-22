2d517f6e5f3ea400dfc33f47f721b76a0b682f28
miker704 <miker704@gmail.com>
2022-04-22 12:48:31 -0400

    added benchmark to subsum

--- Execution_time_differences_algorithms/largest_contiguous_sub_sum.rb
+++ Execution_time_differences_algorithms/largest_contiguous_sub_sum.rb
@@ -7,33 +7,39 @@ require 'benchmark'
 
 # Example:
 
-    list = [5, 3, -7]
-    largest_contiguous_subsum(list) # => 8
+#     list = [5, 3, -7]
+#     largest_contiguous_subsum(list) # => 8
 
-    # possible sub-sums
-    [5]           # => 5
-    [5, 3]        # => 8 --> we want this one
-    [5, 3, -7]    # => 1
-    [3]           # => 3
-    [3, -7]       # => -4
-    [-7]          # => -7
-# Example 2:
+#     # possible sub-sums
+#     [5]           # => 5
+#     [5, 3]        # => 8 --> we want this one
+#     [5, 3, -7]    # => 1
+#     [3]           # => 3
+#     [3, -7]       # => -4
+#     [-7]          # => -7
+# # Example 2:
 
-    list = [2, 3, -6, 7, -6, 7]
-    largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
+    # list = [2, 3, -6, 7, -6, 7]
+    # largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
 # Example 3:
 
-    list = [-5, -1, -3]
-    largest_contiguous_subsum(list) # => -1 (from [-1])
+    # list = [-5, -1, -3]
+    # largest_contiguous_subsum(list) # => -1 (from [-1])
 
     # Phase I
     # Write a function that iterates through the array 
     # and finds all sub-arrays using nested loops. First make an
     #  array to hold all sub-arrays. Then find the sums of each 
     #  sub-array and return the max.
-    
     # Discuss the time complexity of this solution.
     
+    def largest_contiguous_subsum(list)
+
+
+    end
+
+
+
     # Phase II
     # Let's make a better version. Write a new function using O(n) time with O(1) memory. 
     # Keep a running tally of the largest sum. To accomplish this 
@@ -43,4 +49,18 @@ require 'benchmark'
     
     # Get your story straight, and then explain your solution's 
     # time complexity to your TA.
+    list = [2, 3, -6, 7, -6, 7]
+    list2 = [5, 3, -7]
+    list3 = [-5, -1, -3]
+    random_list=(0..1000).to_a.shuffle
+    Benchmark.bmbm do |x|
+        x.report("largest_contigous_subsum(list)") { largest_contiguous_subsum(list) }
+        x.report("largest_contigous_subsum(list2)") { largest_contiguous_subsum(list2) }
+        x.report("largest_contigous_subsum(list3)") { largest_contiguous_subsum(list3) }
+        x.report("largest_contigous_subsum(random_list)") { largest_contiguous_subsum(random_list) }
+
+        
+    
+    
+      end
 
